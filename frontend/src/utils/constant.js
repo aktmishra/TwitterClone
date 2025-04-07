ebe47c1624ea4d47a1a31c1ed29ecdd2a4d5274d
Ankit Mishra <aktmishra72@gmail.com>
2025-04-07 18:26:00 +0530

    endpoint test

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT = "https://twitter-clone-wuyj.vercel.app/user";
-export const TWEET_API_END_POINT = "https://twitter-clone-wuyj.vercel.app/tweet";
+export const USER_API_END_POINT = "https://twitter-clone-wuyj.vercel.app/api/v1/user";
+export const TWEET_API_END_POINT = "https://twitter-clone-wuyj.vercel.app/api/v1/tweet";
 
- 
+// https://twitter-clone-wuyj.vercel.app/
